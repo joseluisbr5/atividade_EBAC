cfd93d744f1acccc5d230286228f22fccf9b3678
José Luis <joseluis.123.br@gmail.com>
2024-12-09 12:11:11 -0300

    final

---
+++ atividade_typescript/atividadets.ts
@@ -0,0 +1,13 @@
+// Função de multiplicação
+function multiplicar(a: number, b: number): number {
+    return a * b;
+}
+
+// Função de saudação
+function saudar(nome: string): string {
+    return "Olá, " + nome;
+}
+
+// Testando as funções
+console.log(multiplicar(3, 4)); // Saída: 12
+console.log(saudar("José"));   // Saída: Olá José
