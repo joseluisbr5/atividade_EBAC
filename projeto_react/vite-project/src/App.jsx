7ecd0f58b9767dc926c222efc6a8caf7ac38e96f
José Luis <joseluis.123.br@gmail.com>
2025-01-20 13:25:45 -0300

    final 2

---
+++ projeto_react/vite-project/src/App.jsx
@@ -0,0 +1,15 @@
+
+import Formulario from "./components/Formulario";
+
+import { useState } from "react";
+function App() {
+ return (
+  <div>
+    
+    <Formulario />
+ 
+  </div>
+) 
+}
+
+export default App
